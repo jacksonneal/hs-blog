27af7df584446ca18032b8f90c963600066a1e4c
jacksonneal <jacksontneal@gmail.com>
2023-02-05 00:43:11 -0500

    progress

--- hello.hs
+++ hello.hs
@@ -1,1 +1,14 @@
-main = putStrLn "<html><body>Hello, world!</body></html>"
+import Html
+
+myhtml :: Html
+myhtml =
+  html_
+    "My page title"
+    ( h1_ "Welcome to the machine"
+        <> ( ul_ [p_ "Let's hack"]
+               <> p_ "Let's hack"
+           )
+    )
+
+main :: IO ()
+main = putStrLn (render myhtml)
