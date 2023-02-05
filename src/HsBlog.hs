2900cdfa90b157808ca88c97ccaa76316d520fcf
jacksonneal <jacksontneal@gmail.com>
2023-02-05 14:07:57 -0500

    cli

--- src/HsBlog.hs
+++ src/HsBlog.hs
@@ -1,47 +1,22 @@
-module HsBlog where
+module HsBlog
+  ( convertSingle,
+    convertDirectory,
+    process,
+  )
+where
 
 import HsBlog.Convert (convert)
-import qualified HsBlog.Html
-import qualified HsBlog.Markup
-import System.Directory (doesFileExist)
-import System.Environment (getArgs)
-import System.IO (getContents')
+import qualified HsBlog.Html as Html
+import qualified HsBlog.Markup as Markup
+import System.IO
 
-process :: HsBlog.Html.Title -> String -> String
-process title = HsBlog.Html.render . convert title . HsBlog.Markup.parse
+convertSingle :: Html.Title -> Handle -> Handle -> IO ()
+convertSingle title input output = do
+  content <- hGetContents input
+  hPutStrLn output (process title content)
 
-main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [] -> do
-      content <- getContents'
-      putStrLn (process "Empty title" content)
-    [input, output] ->
-      do
-        content <- readFile input
-        exists <- doesFileExist output
-        let writeResult = writeFile output (process input content)
-        if exists
-          then whenIO confirm writeResult
-          else writeResult
-    _ ->
-      putStrLn "Usage: runghc Main.hs [-- <input-file> <output-file]"
+convertDirectory :: FilePath -> FilePath -> IO ()
+convertDirectory = error "Not implemented"
 
-confirm :: IO Bool
-confirm = do
-  putStrLn "Are you sure? (y/n)"
-  answer <- getLine
-  case answer of
-    "y" -> pure True
-    "n" -> pure False
-    _ -> do
-      putStrLn "Invalid response. use y or n"
-      confirm
-
-whenIO :: IO Bool -> IO () -> IO ()
-whenIO cond action = do
-  result <- cond
-  if result
-    then action
-    else pure ()
+process :: Html.Title -> String -> String
+process title = Html.render . convert title . Markup.parse
